14d663beae4422cf50f515e96896aceeac064217
Akash <57212963+Akash-152000@users.noreply.github.com>
2021-07-29 15:35:10 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import './css/card.css';
 import dollar from "./dollar.png"
+import add from "./plus.png"
 
 class Card extends Component {
+
+    constructor(props){
+        super(props)
+        this.state={
+        }
+    }
     render() {
         return (
             <div className="Card">
-                <img className="logo" src={this.props.logo}/>
+                <img className="logo" src={this.props.logo} alt="Icons"/>
                 <p className="name">{this.props.name}</p>
                 <div className="amount">
-                    <img className="dollar" src={dollar}/>
+                    <img className="dollar" src={dollar} alt="Dollar"/>
                     <p className="price">{this.props.price}</p>
                 </div>
+                <img className="add" src={add} alt="add item" onClick={()=>{this.props.handleClick(this.props.name,this.props.price)}}/>
             </div>
         );
     }
