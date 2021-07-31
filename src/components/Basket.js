25bc77d7a2aacbee6d7ff972e46bbd5c4cac2432
Akash <57212963+Akash-152000@users.noreply.github.com>
2021-07-31 12:19:44 +0530

    Update Basket.js

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import './css/basket.css'
+import minus from './minus.png'
 
 class Basket extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            // items:[]
+        }
+    }
+
     render() {
         return (
             <div className="basket">
-                {this.props.myBasket.map(item=>(
-                    <li>{item.name}</li>
-                ))}
+                    <p><img src={minus}/>{((JSON.stringify(this.props.items.reduce((acc, val) => {
+                        acc[val] = acc[val] === undefined ? 1 : acc[val] += 1;
+                        return acc;
+                      }, {})).replace(/[{}]/g, '')).replace(/["]/g,'')).replace(/[,]/g,"\n")  }{"\n"}</p><br></br>
+                
             </div>
         );
     }
